6431293c84057fb0ece2ae80c7777e7c8d527ccc
henry <hboh@seoulauctionblue.com>
2025-12-30 23:34:18 +0900

    기타안내

--- core/header.js
+++ core/header.js
@@ -239,9 +239,19 @@ const defaultHeader = new HeaderManager()
       max-width: 100% !important;
       height: auto;
     }
-    * {
-      box-sizing: border-box;
-    }
+      select {
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+        background: url("data:image/svg+xml,%3Csvg width='12' height='8' viewBox='0 0 12 8' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1 1.5L6 6.5L11 1.5' stroke='%23A10076' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3C/svg%3E") no-repeat 95% center;
+        background-size: 12px auto;
+      }
+      select::-ms-expand {
+        display: none;
+      }
+      * {
+        box-sizing: border-box;
+      }
   `
   )
   .addTailwindStyle(
